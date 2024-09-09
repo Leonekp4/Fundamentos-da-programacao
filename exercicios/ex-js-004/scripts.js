3f4fe99bfcb96bc4b97af660f689317893eff894
Leone Santana <leone.dev5@hotmail.com>
2024-09-09 20:52:40 -0300

    iniciando

---
+++ exercicios/ex-js-004/scripts.js
@@ -0,0 +1,14 @@
+console.log("Hello World!")
+console.log("Esta mensagem é do JS conectado com o HTML")
+console.log("Esta mensagem é do JS conectado com o HTML")
+
+
+let balance = 500
+let payment = 120
+
+console .log(balance - payment)
+console .log(balance > payment)
+console .log(balance < payment)
+console .log(balance <= payment)
+console .log(balance >= payment)
+
